93639ce4ecc3f1a038f40c81f0dcecb5ab7790fb
Francesco Bruschi <fbrusch@gmail.com>
2022-10-26 11:18:21 +0200

    Move drawing into a function, and introduce "world state"

--- code/visualization/index.js
+++ code/visualization/index.js
@@ -3,9 +3,20 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
-// now let's draw a rectangle
+// we move drawing into a function, and define a "state" of the rectangle
 
-context.moveTo(250, 200);
-context.beginPath();
-context.rect(20,20,150,150);
-context.stroke();
+var x = 0;
+var y = 0;
+var w = 150;
+var h = 150;
+
+function render() {
+    //clear canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    
+    context.beginPath();
+    context.rect(x,y,w,h);
+    context.stroke();
+}
+
+render();
